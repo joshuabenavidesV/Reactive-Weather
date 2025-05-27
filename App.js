beb8c9bb77348ed05f0dda3f3e05d7d5cee8a779
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-27 12:10:30 -0500

    imported weathercard and cities array, mapped through the array and added weathercard.Shows same as example.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
-
+import WeatherCard from './components/WeatherCard'
+import cities from './data'
 
 function App() {
     return (
@@ -9,7 +10,9 @@ function App() {
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
                 {/* Render components here */}
-                
+                {cities.map((city,index) => ( 
+                    <WeatherCard key={index} data={city} />
+                ))}
 
 
             </div>
